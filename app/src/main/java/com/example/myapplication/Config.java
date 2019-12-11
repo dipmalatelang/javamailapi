748309f16ceca09d4e6f7d764d9160568557ac73
deeep <telangdipmala@gmail.com>
2019-12-11 14:02:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Config.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+public class Config {
+
+    public static final String EMAIL ="your-gmail-username";
+    public static final String PASSWORD ="your-gmail-password";
+}
