9364d9007d65b951b61619c06cdf6a8a60bbd4c8
tejaspatel <tpatel0279@gmail.com>
2019-12-13 10:19:16 +0530

    removed dependency added .jar to libs working properly testing remains..

--- app/src/main/java/com/example/myapplication/Config.java
+++ app/src/main/java/com/example/myapplication/Config.java
@@ -2,6 +2,6 @@ package com.example.myapplication;
 
 public class Config {
 
-    public static final String EMAIL ="your-gmail-username";
-    public static final String PASSWORD ="your-gmail-password";
+    public static final String EMAIL ="tej72793@gmail.com";
+    public static final String PASSWORD ="Uzumymwlxgiwyl";
 }
